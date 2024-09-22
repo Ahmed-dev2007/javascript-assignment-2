a98e03469634f5541a37c61bfc2da3fc09308e53
Ahmed <Ahmedoffical.110@gmail.com>
2024-09-22 19:25:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,143 @@
+// var vowel = prompt("enter a vowel alphabet")
+// switch(vowel){
+//     case "a":
+//         alert("vowel")
+//         break;    
+//     case "e":
+//         alert("vowel")
+//         break;
+    
+//     case "i":
+//         alert("vowel")
+//         break;
+    
+//     case "o":
+//         alert("vowel")
+//         break;
+    
+//     case "u":
+//         alert("vowel")
+//         break;
+    
+//         default:
+//             alert("WRONG ALPHABET")
+//             break;
+// }
+
+
+
+
+// var stu=["ahmed","ali","raza"];
+// stu.pop("")
+// console.log(stu);
+
+
+// var stu=["ahmed","ali","raza"];
+// stu.push("nassu");
+// console.log(stu);
+
+
+// var stu=["ahmed","ali","raza"];
+// stu.shift("");
+// console.log(stu);
+
+
+// var stu=["ahmed","ali","raza"];
+// stu.unshift("alex");
+// console.log(stu);
+
+
+
+// Assignment
+
+
+// Question 1
+
+// let num1 = parseFloat(prompt("Enter the first number:"));
+// let num2 = parseFloat(prompt("Enter the second number:"));
+// let operator = prompt("Enter an operator (+, -, *, /):");
+
+// let result;
+
+// if (operator === "+") {
+//     result = num1 + num2;
+// } else if (operator === "-") {
+//     result = num1 - num2;
+// } else if (operator === "*") {
+//     result = num1 * num2;
+// } else if (operator === "/") {
+//     if (num2 !== 0) {
+//         result = num1 / num2;
+//     } else {
+//         result = "Error! Division by zero.";
+//     }
+// } else {
+//     result = "Invalid operator!";
+// }
+
+// alert("Result: " + result);
+
+
+// Question 2
+
+// let number = parseInt(prompt("Enter a number 1 to 7:"));
+// let day;
+
+// if (number === 1) {
+//     day = "Monday";
+// } else if (number === 2) {
+//     day = "Tuesday";
+// } else if (number === 3) {
+//     day = "Wednesday";
+// } else if (number === 4) {
+//     day = "Thursday";
+// } else if (number === 5) {
+//     day = "Friday";
+// } else if (number === 6) {
+//     day = "Saturday";
+// } else if (number === 7) {
+//     day = "Sunday";
+// } else {
+//     day = "Invalid input!";
+// }
+
+// alert("Day: " + day);
+
+
+// Question 3
+
+// let age = parseInt(prompt("Enter your age:"));
+// let category;
+
+// if (age < 12) {
+//     category = "Child";
+// } else if (age >= 12 && age <= 17) {
+//     category = "Teenager";
+// } else if (age >= 18 && age <= 59) {
+//     category = "Adult";
+// } else if (age >= 60) {
+//     category = "Senior";
+// } else {
+//     category = "Invalid input!";
+// }
+
+// alert("You are a: " + category);
+
+
+// Question 3
+
+// let password = prompt("Enter your password:");
+// let strength;
+
+// if (password.length < 6) {
+//     strength = "Weak";
+// } else if (password.length >= 6 && password.length <= 10) {
+//     strength = "Moderate";
+// } else if (password.length > 10) {
+//     strength = "Strong";
+// } else {
+//     strength = "Invalid input!";
+// }
+
+// alert("Password strength: " + strength);
+
